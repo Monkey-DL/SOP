2d54225669d8cdfce07d7fabeac4e3da45d552c7
unknown <C:\Program Files\The Bat!\Mail>
2019-06-12 16:32:59 +0300

    сделал раздел информации о ребёнке

--- client/js/children.js
+++ client/js/children.js
@@ -1,9 +1,33 @@
-class children {
+class Children {
     constructor() {
-        let firstName;
-        let lastName;
-        let patronymic;
-        let birthday;
-        
-    }
+        this.id;
+        this.firstName = "Егор";
+        this.lastName = "Матвеев";
+        this.patronymic;
+        this.birthday;
+
+        this.school;
+        this.classNumber;
+        this.placeOfStudy;
+
+        this.homeNumber;
+        this.mobileNumber;
+
+        this.villageCouncil;
+        this.locality;
+        this.address;
+
+        this.sopRecognitionDates = [];
+        this.sopReviewDate;
+        this.termControl;
+        this.norm;
+
+        this.notes;
+
+        this.trustees = [];
+    };
+
+    getFullName() {
+        return this.lastName + " " + this.firstName;
+    };
 }
--- client/js/index.js
+++ client/js/index.js
@@ -1,29 +1,28 @@
-let childrens = ["Макаров Илай", "Дюжев Виталий", "Манкин Светослав"];
+let childrens = [];
+
+for(var i=0;i<5;i++){
+    childrens.push(new Children());    
+}
 
 var childrenList = document.getElementById("childrenList");
 
-
-
+function newButton(item, className){
+    var Button = document.createElement("div");
+    Button.classList = "button "+ className;
+    return Button;
+}
 
 for (let i = 0; i < childrens.length; i++) {
     var childrenItem = document.createElement("div");
     childrenItem.classList = "children_list_item";
 
     var childrenItemText = document.createElement("span");
-    childrenItemText.innerHTML = childrens[i];
+    childrenItemText.innerHTML = childrens[i].getFullName();
     childrenItem.appendChild(childrenItemText);
-
-    var changeButton = document.createElement("div");
-    changeButton.classList = "button change";
-    childrenItem.appendChild(changeButton);
-
-    var toListButton = document.createElement("div");
-    toListButton.classList = "button to_list";
-    childrenItem.appendChild(toListButton);
-
-    var deleteButton = document.createElement("div");
-    deleteButton.classList = "button delete";
-    childrenItem.appendChild(deleteButton);
-
+    
+    childrenItem.appendChild(newButton(childrenItem,"change"));
+    childrenItem.appendChild(newButton(childrenItem,"to_list"));
+    childrenItem.appendChild(newButton(childrenItem,"delete"));
+    
     childrenList.appendChild(childrenItem);
 };
